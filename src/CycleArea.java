a9546f0ac0aed7054ba1bdb5fed8fba7f26e1c8f
Ahmed Khaled Elsharkawy <36075516+sharkawy98@users.noreply.github.com>
2020-03-02 22:51:31 +0200

    Update CycleArea.java

--- src/CycleArea.java
+++ src/CycleArea.java
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package main;
-//import javax.swing.text.html.parser.Parser;
-/**
- *
- * @author Eslam ismail
- * 
- */
-
 public class CycleArea implements ISubscriber {
     public CycleArea() {
 		// TODO Auto-generated constructor stub

19cdf9b3994c83f0b976eccac70754be6c95b366
Mustafa badr <36304069+moustafaBadrr@users.noreply.github.com>
2020-03-03 15:33:24 +0200

    20170286
    Fibonacci is done

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -9,6 +9,8 @@ public class Fibonacci implements ISubscriber{
 	@Override
 	public void notifySubscriber(String input) {
 		// TODO Auto-generated method stub
+		int input2 = Integer.parseInt(input);
+		System.out.println("The Result of Fibonacci of " + input +" is equal : "+ fibonacci(input2));
 		
 	}
 }
